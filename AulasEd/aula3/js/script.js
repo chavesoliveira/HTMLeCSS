aaf1c6603dc63c37feed6cd41640595eb777bbba
Rodrigo Chaves <87891153+chavesoliveira@users.noreply.github.com>
2021-10-18 00:43:59 -0300

    Initial commit

---
+++ AulasEd/aula3/js/script.js
@@ -0,0 +1,6 @@
+		var a = 10
+		var b = 8
+		var c = "Texto" //string é todo texto no js
+		var d = 5.2 //tipo float (flutuar), variavel do tipo com .flutuante
+		var e = ["Maçã", "Caqui", "Pêra", "Laranja"] //array lista de alguma coisa
+		 //concatenar é a forma de juntar texto com N interiro q vira uma string
